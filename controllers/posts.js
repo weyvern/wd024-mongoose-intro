0c3b1d3963fc6a4b5c43c2ddaf5697a75a79b621
Jorge Paul <jserranopaul@gmail.com>
2022-04-07 12:31:30 +0200

    GET single, PUT, DELETE

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,8 +13,30 @@ export const createPost = asyncHandler(async (req, res) => {
   res.status(201).json(newPost);
 });
 
-export const getSinglePost = asyncHandler(async (req, res) => res.send('GET single'));
+export const getSinglePost = asyncHandler(async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  const post = await Post.findById(id);
+  if (!post) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  res.send(post);
+});
 
-export const updatePost = asyncHandler(async (req, res) => res.send('PUT'));
+export const updatePost = asyncHandler(async (req, res) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+  const updatedPost = await Post.findOneAndUpdate({ _id: id }, body, { new: true });
+  if (!updatedPost) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  res.json(updatedPost);
+});
 
-export const deletePost = asyncHandler(async (req, res) => res.send('DELETE'));
+export const deletePost = asyncHandler(async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  const deleted = await Post.findByIdAndDelete(id);
+  if (!deleted) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  res.json({ success: `Post with id of ${id} was deleted` });
+});
