5c16221b06ab1b8106f1af95af3450f00f351a79
Jorge Paul <jserranopaul@gmail.com>
2022-04-07 09:28:19 +0200

    Scaffold

---
+++ controllers/posts.js
@@ -0,0 +1,9 @@
+export const getAllPosts = async (req, res, next) => res.send('GET all');
+
+export const createPost = async (req, res) => res.send('POST');
+
+export const getSinglePost = async (req, res) => res.send('GET single');
+
+export const updatePost = async (req, res) => res.send('PUT');
+
+export const deletePost = async (req, res) => res.send('DELETE');
